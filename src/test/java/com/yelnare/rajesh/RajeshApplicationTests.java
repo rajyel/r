77f1e87a5259574bf9088cb07e3a6e5eacbfd73a
rajyel@gmail.com <rajyel@gmail.com>
2021-09-14 22:08:37 -0400

    first commit

---
+++ src/test/java/com/yelnare/rajesh/RajeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yelnare.rajesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
